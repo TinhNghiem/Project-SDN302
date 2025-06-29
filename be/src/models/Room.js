8c062026cc6c7734c1c0232ec151f178d5e4daca
quockhanh201203 <khanhnqhe171412@fpt.edu.vn>
2025-06-29 18:51:03 +0700

    backend and frontend

--- be/src/models/Room.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true }
-});
-
-module.exports = mongoose.model('Room', roomSchema);
